0d72ad098eb4888644f44a4c827e83166cab19dc
Grégory <greg.eres.07@hotmail.com>
2023-09-15 15:43:49 +1100

    ajout des hook d'activation

--- QuestMaster.php
+++ QuestMaster.php
@@ -19,6 +19,26 @@ function mon_plugin_enqueue_scripts() {
     wp_enqueue_style('mon-plugin-style', plugin_dir_url(__FILE__) . 'assets/css/style.css', array(), '1.0');
 }
 add_action('wp_enqueue_scripts', 'mon_plugin_enqueue_scripts');
+
+function mon_plugin_menu() {
+    // Ajoutez des éléments de menu ou des pages d'administration ici, si nécessaire
+}
+add_action('admin_menu', 'mon_plugin_menu');
+
+// Actions WordPress pour traiter les fonctions personnalisées, si nécessaire
+function mon_plugin_custom_function() {
+    // Ajoutez vos fonctions personnalisées ici, si nécessaire
+}
+
+// Activation et désactivation du plugin
+function mon_plugin_activate() {
+    // Effectuez des actions lors de l'activation du plugin, si nécessaire
+}
+
+function mon_plugin_deactivate() {
+    // Effectuez des actions lors de la désactivation du plugin, si nécessaire
+}
+
 function game_display() {
     ob_start();
     ?>
